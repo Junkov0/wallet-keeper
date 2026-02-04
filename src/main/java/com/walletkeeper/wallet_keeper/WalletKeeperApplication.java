d969f3822080f4e1bc7ad0f6613c6eb6e287b21a
Junkov0 <devjuncloud@gmail.com>
2026-02-04 19:47:18 +0900

    first commit

---
+++ src/main/java/com/walletkeeper/wallet_keeper/WalletKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.walletkeeper.wallet_keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walletkeeper/wallet_keeper/WalletKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walletkeeper.wallet_keeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
